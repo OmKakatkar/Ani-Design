9f909dedf84fe2db445602c3868052e988d192d2
Om Kakatkar <68369994+OmKakatkar@users.noreply.github.com>
2022-04-07 20:47:01 +0530

    Docs updates fixes (#17)

--- js/modal.js
+++ js/modal.js
@@ -1,21 +1,35 @@
+// Simple Modal
 const showSimpleModal = document.querySelector('#show-simple-modal');
-const showModalWithButton = document.querySelector('#show-modal-with-button');
-const showModalList = document.querySelector('#show-modal-list');
-
 const simpleModal = document.querySelector('#simple-modal');
 const closeSimpleModal = document.querySelector('#close-simple-modal');
-const modalWithButton = document.querySelector('#modal-with-button');
-const closeModalWithButton = document.querySelector('#close-modal-with-button');
-const modalList = document.querySelector('#modal-list');
-const closeModalList = document.querySelector('#close-modal-list');
-
 showSimpleModal.addEventListener('click', () => toggleModal(simpleModal));
 closeSimpleModal.addEventListener('click', () => toggleModal(simpleModal));
-showModalWithButton.addEventListener('click', () => toggleModal(modalWithButton));
-closeModalWithButton.addEventListener('click', () => toggleModal(modalWithButton));
+
+// Modal With Button
+const showModalWithButton = document.querySelector('#show-modal-with-button');
+const modalWithButton = document.querySelector('#modal-with-button');
+const closeModalWithButton = document.querySelector('#close-modal-with-button');
+showModalWithButton.addEventListener('click', () =>
+	toggleModal(modalWithButton)
+);
+closeModalWithButton.addEventListener('click', () =>
+	toggleModal(modalWithButton)
+);
+
+// Modal List
+const modalList = document.querySelector('#modal-list');
+const closeModalList = document.querySelector('#close-modal-list');
+const showModalList = document.querySelector('#show-modal-list');
 showModalList.addEventListener('click', () => toggleModal(modalList));
 closeModalList.addEventListener('click', () => toggleModal(modalList));
 
+// Utility functions for Opening and Closing the modals
 const toggleModal = modalId => {
 	modalId.classList.toggle('hidden');
 };
+
+window.addEventListener('click', e => {
+	if (e.target.classList.contains('modal')) {
+		e.target.classList.add('hidden');
+	}
+});
--- js/toast.js
+++ js/toast.js
@@ -1,47 +1,48 @@
-const showToastBottomLeft = document.querySelector('#show-toast-bottom-left');
-const ToastBottomLeft = document.querySelector('#toast-bottom-left');
-
-const showToastBottomCenter = document.querySelector(
-	'#show-toast-bottom-center'
-);
-const ToastBottomCenter = document.querySelector('#toast-bottom-center');
-
-const showToastBottomRight = document.querySelector('#show-toast-bottom-right');
-const ToastBottomRight = document.querySelector('#toast-bottom-right');
-
+// Toast Top Left
 const showToastTopLeft = document.querySelector('#show-toast-top-left');
 const ToastTopLeft = document.querySelector('#toast-top-left');
-
-const showToastTopCenter = document.querySelector('#show-toast-top-center');
-const ToastTopCenter = document.querySelector('#toast-top-center');
-
-const showToastTopRight = document.querySelector('#show-toast-top-right');
-const ToastTopRight = document.querySelector('#toast-top-right');
-
-showToastBottomLeft.addEventListener('click', () => {
-	showToast(ToastBottomLeft);
-});
-
-showToastBottomCenter.addEventListener('click', () => {
-	showToast(ToastBottomCenter);
-});
-
-showToastBottomRight.addEventListener('click', () => {
-	showToast(ToastBottomRight);
-});
-
 showToastTopLeft.addEventListener('click', () => {
 	showToast(ToastTopLeft);
 });
 
+// Toast Top Center
+const showToastTopCenter = document.querySelector('#show-toast-top-center');
+const ToastTopCenter = document.querySelector('#toast-top-center');
 showToastTopCenter.addEventListener('click', () => {
 	showToast(ToastTopCenter);
 });
 
+// Toast Top Right
+const showToastTopRight = document.querySelector('#show-toast-top-right');
+const ToastTopRight = document.querySelector('#toast-top-right');
 showToastTopRight.addEventListener('click', () => {
 	showToast(ToastTopRight);
 });
 
+// Toast Bottom Left
+const showToastBottomLeft = document.querySelector('#show-toast-bottom-left');
+const ToastBottomLeft = document.querySelector('#toast-bottom-left');
+showToastBottomLeft.addEventListener('click', () => {
+	showToast(ToastBottomLeft);
+});
+
+// Toast Bottom Center
+const showToastBottomCenter = document.querySelector(
+	'#show-toast-bottom-center'
+);
+const ToastBottomCenter = document.querySelector('#toast-bottom-center');
+showToastBottomCenter.addEventListener('click', () => {
+	showToast(ToastBottomCenter);
+});
+
+// Toast Bottom Right
+const showToastBottomRight = document.querySelector('#show-toast-bottom-right');
+const ToastBottomRight = document.querySelector('#toast-bottom-right');
+showToastBottomRight.addEventListener('click', () => {
+	showToast(ToastBottomRight);
+});
+
+// Utility Function to toggle Toast
 const showToast = toastId => {
 	toastId.classList.toggle('hidden');
 	setTimeout(() => {
